f7d420b93c8eb60898ec8319b9d38fc2ce5a626c
Ime Prezime <moj_email@gmail.com>
2020-02-11 12:12:02 +0100

    Zadatak 4

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public function create () {
 
         return view('register.create');
